cbce8da949680ae6968538a85abe798451bf8290
Tyler Randall <tyler7r@gmail.com>
2023-09-11 15:27:36 -0400

    Reverted changes that were made to navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,24 +1,21 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { BsSearch, BsEnvelopeFill } from "@qwikest/icons/bootstrap";
 
 import { SiteLogo } from "~/components/site-logo";
 import { Button } from "~/components/button";
 import { TeamLogo } from "~/components/team-logo";
 
-import styles from "./navbar.module.css?inline";
+import styles from "./navbar.module.css";
 
 export const Navbar = component$(() => {
-  useStylesScoped$(styles);
-
   return (
     <nav>
-      <a href="/">
+      <a href="/" class={styles["link"]}>
         <SiteLogo />
       </a>
       <Button>Film Room</Button>
-      {/* buttons are causing annoying warnings in console */}
-      {/* <BsSearch />
-      <BsEnvelopeFill /> */}
+      <BsSearch />
+      <BsEnvelopeFill />
       <TeamLogo team="Atlanta Hustle" />
     </nav>
   );
