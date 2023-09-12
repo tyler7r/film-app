ea14100595090a2b3cd4fd423dc7be08c12beadf
tyler7r <tyler7r@gmail.com>
2023-09-12 09:48:51 -0400

    Landing page (#7)
    * Created skeleton layout for home page
    
    * Reverted changes that were made to navbar
    
    * Continued styling, styles and color
    
    * Convert to css modules
    
    * Fix module import typo

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -16,6 +16,9 @@ export const RouterHead = component$(() => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
+      {/* use font source to import styles instead */}
+      {/* <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;600&family=Kanit:ital,wght@0,200;0,400;1,100;1,400&family=Libre+Franklin:wght@400;600&family=Montserrat:wght@400;600&family=Open+Sans:wght@500&display=swap" rel="stylesheet" /> */}
+
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
       ))}
