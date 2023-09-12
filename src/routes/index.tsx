9f07b4c43e3dd4c10f4ff02b6fd4a0b0284e8dc8
Tyler Randall <tyler7r@gmail.com>
2023-09-11 20:58:58 -0400

    Convert to css modules

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,23 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import HomeStyles from './home.css?inline';
+import styles from './home.module.css';
 
 export default component$(() => {
-  useStylesScoped$(HomeStyles);
-
   return (
     <main>
       <h2>Welcome, (user)</h2>
-      <div class='continue-watching'>
-        <div class='continue-watching-title'>Continue Watching</div>
-        <div class='continue-watching-game'>(game info, timestamp, etc...)</div>
+      <div class={styles['continue-watching']}>
+        <div class={styles['continue-watching-title']}>Continue Watching</div>
+        <div class={styles['continue-watching-game']}>(game info, timestamp, etc...)</div>
       </div>
-      <div class='content'>
-        <div class='content-card'>
+      <div class={styles['content']}>
+        <div class={styles['content-card']}>
           Assigned Clips
         </div>
-        <div class='content-card'>
+        <div class={styles['content-card']}>
           Scouting Tab
         </div>
-        <div class='content-card'>
+        <div class={styles['content-card']}>
           Highlight Feed
         </div>
       </div>
