1eedde86689f16d1f862158d99c736af7578274d
Tyler Randall <tyler7r@gmail.com>
2023-09-13 13:45:55 -0400

    Style search bar and submit button

--- src/routes/search/index.tsx
+++ src/routes/search/index.tsx
@@ -1,9 +1,26 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useSignal, useStore } from "@builder.io/qwik";
+
+import styles from './search.module.css'
+import { BsSearch } from "@qwikest/icons/bootstrap";
 
 export default component$(() => {
+    const search = useSignal('');
+
+    const submit = $(() => {
+        console.log(search.value);
+        search.value = ''
+    })
+
     return (
         <div class='content'>
-            
+            <div class={styles['container']}>
+                <form preventdefault:submit onSubmit$={submit} class={styles['search-form']}>
+                    <input class={styles['search-input']} type='text' value={search.value} onInput$={(e) => search.value = (e.target as HTMLInputElement).value} />
+                    <button class={styles['search-icon']}>
+                        <BsSearch />
+                    </button>
+                </form>
+            </div>
         </div>
     )
 })
