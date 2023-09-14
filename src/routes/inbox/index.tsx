b4a2772f532bddf0285d9b2c16a5780afed3250a
Tyler Randall <tyler7r@gmail.com>
2023-09-14 11:49:53 -0400

    Create skeleton setup for inbox route

---
+++ src/routes/inbox/index.tsx
@@ -0,0 +1,29 @@
+import { component$ } from "@builder.io/qwik";
+import styles from './inbox.module.css'
+
+import mockData from '../../../data/db.json';
+
+const inbox = component$(() => {
+    return (
+        <div class={styles['inbox-container']}>
+            <div class={styles['results']}>
+                <div class={styles['results-title']}>Team Announcements</div>
+                <div class={styles['result']}>New Announcement!</div>
+            </div>
+            <div class={styles['results']}>
+                <div class={styles['results-title']}>Mentions</div>
+                <div class={styles['result']}>New Mention</div>
+            </div>
+            <div class={styles['results']}>
+                <div class={styles['results-title']}>Comments</div>
+                <div class={styles['result']}>New Comment</div>
+            </div>
+            <div class={styles['results']}>
+                <div class={styles['results-title']}>Highlight Mentions</div>
+                <div class={styles['result']}>New Highlight Mention</div>
+            </div>
+        </div>
+    )
+})
+
+export default inbox
