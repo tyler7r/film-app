772fd67125b64499139cc3e2f5c8817f90d2ffbb
Tyler Randall <tyler7r@gmail.com>
2023-09-19 09:10:02 -0400

    Continued home styling, site logo and nav styling

--- src/components/site-logo/index.tsx
+++ src/components/site-logo/index.tsx
@@ -13,13 +13,13 @@ export const SiteLogo = component$((props) => {
       >
         <path
           d="m35 27.869c-1.364 2.463-3.986 4.131-7 4.131h14c-3.014 0-5.636-1.668-7-4.131"
-          fill="white"
+          fill="#d3ab2b"
         />
-        <circle cx="28" cy="24" r="3" fill="white" />
-        <circle cx="42" cy="24" r="3" fill="white" />
+        <circle cx="28" cy="24" r="3" fill="#d3ab2b" />
+        <circle cx="42" cy="24" r="3" fill="#d3ab2b" />
         <path
           d="m32 2c-16.568 0-30 13.432-30 30s13.432 30 30 30 30-13.432 30-30-13.432-30-30-30m14 30c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2h-23c-1.1 0-2-.9-2-2v-.5l-5 2.5v-16l5 2.5v-.5c0-1.1.9-2 2-2h5c-4.418 0-8-3.582-8-8s3.582-8 8-8c3.014 0 5.636 1.668 7 4.131 1.364-2.463 3.986-4.131 7-4.131 4.418 0 8 3.582 8 8s-3.582 8-8 8h4"
-          fill="white"
+          fill="#d3ab2b"
         />
       </svg>
       <div class={styles['logo-name']}>Film Study</div>
