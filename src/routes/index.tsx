782afa90a613c7c7d4a7a1f18abef8c2b31b9307
Tyler Randall <tyler7r@gmail.com>
2023-09-19 14:29:15 -0400

    Styled scouting tab and assigned clips tag

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,12 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import styles from './index.module.css';
+import mockData from '../../data/db.json'
 
 export default component$(() => {
+  const exampleTeam = mockData.teams[1];
+  const exampleGame = mockData.games[1];
+
   return (
     <main class='content'>
       <h2 class={styles['welcome-msg']}>Welcome, Tyler Randall!</h2>
@@ -10,15 +14,34 @@ export default component$(() => {
         <div class={styles['continue-watching-title']}>Continue Watching</div>
         <div class={styles['continue-watching-game']}>(game info, timestamp, etc...)</div>
       </div>
-      <div class={styles['content']}>
+      <div class={styles['content-cards']}>
         <div class={styles['content-card']}>
-          Assigned Clips
+          <div class={styles['card-title']}>Assigned Clips</div>
+          <div class={styles['card-content']}>
+            <a href='/film-room/g1'><div class={styles['assigned-clip']}>I am an example of an assigned clip!</div></a>
+          </div>
         </div>
         <div class={styles['content-card']}>
-          Scouting Tab
+          <div class={styles['card-title']}>Scouting Tab</div>
+            <div class={styles['card-content']}>
+              <div class={styles['scouting-container']}>
+                <div class={styles['scouting-title']}>Next Opponent</div>
+                <a href='/profile/t1'><div class={styles["scouting-info"]}>
+                  <img src={exampleTeam.logo} class={styles['next-opponent-logo']} alt='opponent-logo' />
+                  <div class={styles['next-opponent-name']}>{exampleTeam.city} {exampleTeam.name}</div>
+                </div></a>
+              </div>
+              <div class={styles['scouting-container']}>
+                <div class={styles['scouting-title']}>Our Last Game</div>
+                <a href='/film-room/g1'><div class={styles["scouting-info"]} id={styles['last-game-info']}>
+                  <div class={styles['last-game-opponent']}>vs. {exampleGame.team2}</div>
+                  <div class={styles['last-game-tournament']}>@{exampleGame.tournament}</div>
+                </div></a>
+              </div>
+            </div>
         </div>
-        <div class={styles['content-card']}>
-          Highlight Feed
+        <div class={styles['content-card']} id={styles['highlight-card']}>
+          <div class={styles['highlight-title']}>The Highlight Factory</div>
         </div>
       </div>
     </main>
