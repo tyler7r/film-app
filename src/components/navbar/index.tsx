258ac204e2388006b1e2969061e2918fcc415997
Tyler Randall <tyler7r@gmail.com>
2023-09-20 16:34:54 -0400

    Add link to login page to nav bar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,5 +1,5 @@
 import { $, component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
-import { BsSearch, BsEnvelopeFill } from "@qwikest/icons/bootstrap";
+import { BsSearch, BsEnvelopeFill, BsGearFill } from "@qwikest/icons/bootstrap";
 
 import { SiteLogo } from "~/components/site-logo";
 import { Button } from "~/components/button";
@@ -25,13 +25,14 @@ export const Navbar = component$(() => {
       {!searchOpen.value
         ? <div class={styles['right']}>
           <a href='/film-room'><Button>Film Room</Button></a>
-          <div class={styles['nav_btn']}>
+          <div class={styles['nav-btn']}>
             <BsSearch onClick$={() => searchOpen.value = true}/>
           </div>
-          <a href='/inbox' class={styles['nav_btn']}>
+          <a href='/inbox' class={styles['nav-btn']}>
             <BsEnvelopeFill />
           </a>
           <a href={`/profile/${teamId}`} class={styles['team-logo']}><TeamLogo team="Atlanta Hustle" /></a>
+          <a href='/login' class={styles['nav-btn']}><BsGearFill /></a>
         </div>
         : <NavSearch closeSearch={closeSearch} />
       }
