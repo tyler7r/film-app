0a2d2b7f25635b10bff64995b643c8e31b0a045c
Tyler Randall <tyler7r@gmail.com>
2023-10-05 13:29:01 -0400

    update film home component

--- src/routes/film-room/index.tsx
+++ src/routes/film-room/index.tsx
@@ -1,28 +1,49 @@
-import { component$ } from "@builder.io/qwik";
-import styles from "./film-home.module.css";
-import mockData from "../../../data/db.json";
-import { Game } from "~/components/game";
+import { Resource, component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import ContentLink from "~/components/content-link";
+import Game from "~/components/game";
 import PageTitle from "~/components/page-title";
+import { supabase } from "../layout";
+import styles from "./film-home.module.css";
+
+const useGetAllGames = routeLoader$(async (requestEv) => {
+  const { data } = await supabase
+    .from("games")
+    .select(`*, team1: team1_id(*), team2: team2_id(*)`);
+  const result = data;
+  return result;
+});
 
 const FilmHome = component$(() => {
-  const games = mockData.games;
-
+  const games = useGetAllGames();
   return (
     <div class={styles["container"]}>
       <PageTitle>Game Select</PageTitle>
       <div class={styles["games-container"]}>
-        {games.map((game) => (
-          <a href={`/film-room/${game.id}`} key={game.id}>
-            <Game
-              id={game.id}
-              team1={game.team1}
-              team2={game.team2}
-              season={game.season}
-              tournament={game.tournament}
-              classProp={styles["game"]}
-            />
-          </a>
-        ))}
+        <Resource
+          onPending={() => <div>Loading...</div>}
+          value={games}
+          onResolved={(games) => (
+            <>
+              {games &&
+                games.map((game: any) => (
+                  <ContentLink
+                    key={`game-link${game.id}`}
+                    href={`/film-room/g${game.id}`}
+                  >
+                    <Game
+                      id={game.id}
+                      season={game.season}
+                      tournament={game.tournament}
+                      team1={game.team1}
+                      team2={game.team2}
+                    />
+                  </ContentLink>
+                ))}
+              {games?.length === 0 && <div>No Games Recorded</div>}
+            </>
+          )}
+        />
       </div>
     </div>
   );
