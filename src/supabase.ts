f2078e11c61a21be61de28dc90a19575bf37b45a
Tyler Randall <tyler7r@gmail.com>
2023-10-06 10:22:34 -0400

    Lint and format

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "./types/generated_types";
+import { type Database } from "./types/generated_types";
 
 export const supabase = createClient<Database>(
   process.env.PUBLIC_SUPABASE_URL!,
