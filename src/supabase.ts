eb00747322825331cf7bb4b81d8190d933b02706
Tyler Randall <tyler7r@gmail.com>
2023-10-06 10:19:57 -0400

    Correctly type supabase and test connection

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "./types/generated_types";
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   process.env.PUBLIC_SUPABASE_URL!,
   process.env.PUBLIC_SUPABASE_ANON_KEY!,
 );
