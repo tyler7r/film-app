6174081752168f1ea83c0d76b16ef97a2aa5b3de
Tyler Randall <tyler7r@gmail.com>
2023-10-09 18:47:43 -0400

    Create regex for validating email

---
+++ src/utils/helpers.ts
@@ -0,0 +1,10 @@
+export const rootDomain = "http://localhost";
+
+// Check email is valid
+export const validateEmail = (email: string) => {
+  const regex = new RegExp(/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/);
+  if (regex.test(email)) {
+    return true;
+  }
+  return false;
+};
